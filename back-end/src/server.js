5256149223212ef0499024b83f3e0dc44696e3fe
kieran-calavan <93680051+kieran-calavan@users.noreply.github.com>
2022-07-23 12:59:05 -0500

    update scripts

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,6 +1,6 @@
 const PORT = process.env.PORT || 5000;
 
-const app = require("./app");
+const app = require("./src/app");
 const knex = require("./db/connection");
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
