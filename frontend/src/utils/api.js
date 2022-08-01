1d023c87aef8d8316d3009bce5b2e07d24841ff5
kieran-calavan <93680051+kieran-calavan@users.noreply.github.com>
2022-08-01 18:34:44 -0500

    initial commit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = "https://kieran-movie-project-backend.herokuapp.com"
-  // process.env.REACT_APP_API_BASE_URL || "http://localhost:3000";
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:3000";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
